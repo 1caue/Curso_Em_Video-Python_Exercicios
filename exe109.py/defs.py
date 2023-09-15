7e01e3885e22e42e06fc50b8d4bc5316baceccf5
Cauê <cauusaantos@gmail.com>
2023-09-15 08:58:35 -0300

    add

--- exe109.py/defs.py
+++ exe109.py/defs.py
@@ -1,12 +1,22 @@
-def met(n):
-   return n / 2
+def met(n=0, formato=False):
+   r = n / 2
+   return r if not formato else moeda(r)
+ 
 
-def dob(n): 
-   return n * 2
+def dob(n=0, formato=False): 
+   r = n * 2
+   return r if not formato else moeda(r)
+  
 
-def dez(n, t):
+def dez(n, t, formato=False):
    r = n + (n * t / 100)
-   return r 
+   return r if formato is False else moeda(r)
+
+
+def dim(preço=0, taxa=0, formato=False):
+   r = preço - (preço * taxa/100)
+   return r if formato is False else moeda(r)
+
 
 def moeda(preço=0, moeda='R$'):
    return f"{moeda}{preço:.2f}".replace('.', ',')
--- exe109.py/exe109.py
+++ exe109.py/exe109.py
@@ -1,6 +1,7 @@
 import defs
 
 n = int(input('Digite o preço: R$'))
-print(f'A metade de {defs.moeda(n)} é {defs.moeda(defs.met(n))}')
-print(f'O dobro de {defs.moeda(n)} é {defs.moeda(defs.dob(n))}')
-print(f'Aumentando 10%, temos {defs.moeda(defs.dez(n, 10))}')
+print(f'A metade de {defs.moeda(n)} é {defs.met(n, True)}')
+print(f'O dobro de {defs.moeda(n)} é {defs.dob(n, True)}')
+print(f'Aumentando 10%, temos {defs.dez(n, 10, True)}')
+print(f'Diminuindo 13% temos {defs.dim(n, 13, True)}')
